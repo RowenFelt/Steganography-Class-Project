b938269a74be9a509f65f21c90d7f4b59b930fc4
rfelt <rfelt@middlebury.edu>
2018-12-03 12:47:40 -0500

    encoding images attempt

---
+++ client/src/Components/imageencode.js
@@ -0,0 +1,109 @@
+// Initial code from GitHub
+// https://gist.github.com/AshikNesin/e44b1950f6a24cfcd85330ffc1713513
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Input} from 'reactstrap';
+
+
+class ImageEncode extends Component {
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      message: '',
+      imageurl: '',
+    };
+  }
+
+  firstFileHandler(file) {
+    console.log(file.name);
+    this.props.startLoad();
+    let contents;
+    const callbackProp = this.props.callback;
+
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      contents = reader.result;
+
+      const request = new Request(
+        '/encodefirst',
+        {
+          method: 'POST',
+          body: contents,
+        },
+      );
+
+      fetch(request)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.status_text);
+          }
+          return response.json();
+        })
+        .then((jsonurl) => {
+          console.log(jsonurl);
+        });
+    };
+  }
+
+  secondFileHandler(file) {
+    console.log(file.name);
+    this.props.startLoad();
+    let contents;
+    const callbackProp = this.props.callback;
+
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      contents = reader.result;
+
+      const request = new Request(
+        '/encodesecond',
+        {
+          method: 'POST',
+          body: contents,
+        },
+      );
+
+      fetch(request)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.status_text);
+          }
+          return response.json();
+        })
+        .then((jsonurl) => {
+          this.setState({imageurl: jsonurl.url});
+        });
+    };
+  }
+
+  render() {
+     const uploadButtonBase = <Input type="file" id="file1" placeholder="Encode" onChange={e => this.firstFileHandler(e.target.files[0], this.props)} />;
+
+   const uploadButtonHidden = <Input type="file" id="file2" placeholder="Encode" onChange={e => this.secondFileHandler(e.target.files[0], this.props)} />;
+
+    return (
+      <div>
+        {uploadButtonBase}
+        {uploadButtonHidden}
+        <h1 align="center" >{this.state.message}</h1>
+        {
+          this.state.imageurl && (<img src={this.state.imageurl}/>)
+        }
+      </div>
+    );
+  }
+}
+
+ImageEncode.propTypes = {
+  callback: PropTypes.func.isRequired,
+  startLoad: PropTypes.func.isRequired,
+};
+
+export default ImageEncode;
--- server/simple_server.py
+++ server/simple_server.py
@@ -37,33 +37,10 @@ def upload_file_decode():
     removeTempFiles()
     with open("uploaded-for-decoding.png", "wb") as binary_file:
         num_bytes_written = binary_file.write(request.data)
-    #if request.method == 'POST':
-    #    # check if the post request has the file part
-    #    if 'file' not in request.files:
-    #        flash('No file part')
-    #        return redirect(request.url)
-    #    file = request.files['file']
-    #    # if user does not select file, browser also
-    #    # submit an empty part without filename
-    #    if file.filename == '':
-    #        flash('No selected file')
-    #        return redirect(request.url)
-    #    if file and allowed_file(file.filename, "decode"):
-    #        filename = secure_filename(file.filename)
-    #        filetrunct = filename.rsplit('.', 1)[0]
-    #        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    #        base = skio.imread(filename)
-    #        decode_filename = filetrunct + "decoded" + ".png"  
-    #        sf.saveDecoded(base, decode_filename)
-    #        return redirect(url_for('uploaded_file',
-    #                                filename=decodedfilename))
-    base = skio.imread('uploaded-for-decoding.png')
+        base = skio.imread('uploaded-for-decoding.png')
     sf.saveDecoded(base, "decoded")
-    #return redirect(url_for('uploaded_file',filename="decoded.png"))
     imageurl = "/decoded.png"
     urldict = {"url": imageurl}
-    #listdict =([]).append(urldict)
-    #jsonStr = json.dumps( 
     return jsonify(urldict)
 
 @app.route('/<filename>')
@@ -71,29 +48,29 @@ def upload_file_decode():
 def uploaded_file(filename):
     return send_from_directory('.',
                                filename)
-@app.route('/encode', methods=['GET', 'POST'])
-def upload_file_encode():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        uploaded_files = request.files.getlist("file[]")
-        print(uploaded_files)
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename, "decode"):
-            filename = secure_filename(file.filename)
-            filetrunct = filename.rsplit('.', 1)[0]
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            base = skio.imread(filename)
-            decode_filename = filetrunct + "decoded" + ".png"  
-            sf.saveDecoded(base, decode_filename)
-            return redirect(url_for('uploaded_file',
-                                    filename=decodedfilename))
+@app.route('/encodefirst', methods=['GET', 'POST'])
+def upload_file_encode_first():
+    if(os.path.exists("base.png"):
+            os.remove("base.png")
+    with open("base.png", "wb") as binary_file:
+        num_bytes_written = binary_file.write(request.data)
+        base = skio.imread('base.png')
+    urldit = {"url": '/base.png'}
+    return jsonify(urldict)
+
+@app.route('/encodesecond', methods=['GET', 'POST'])
+def upload_file_encode_first():
+    if(os.path.exists("hidden.png"):
+            os.remove("hidden.png")
+    with open("hidden.png", "wb") as binary_file:
+        num_bytes_written = binary_file.write(request.data)
+        base = skio.imread('hidden.png')
+    if(os.path.exists("base.png"):
+        base = skio.imread("base.png")
+        hidden = skio.imread("hidden.png")
+        sf.saveEncoded(base, hidden, 'encoded_image.png')
+    urldit = {"url": '/encoded_image.png'}
+    return jsonify(urldict)
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
